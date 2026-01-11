ba57265bfac91dc72dda0f657dd72cc9ea1dcb5e
Bedardi Bhai <153967679+Bedardi@users.noreply.github.com>
2026-01-11 14:49:16 +0530

    Change export to module.exports in config.js

--- api/config.js
+++ api/config.js
@@ -1,11 +1,10 @@
-export default function handler(req, res) {
-  // Ye Vercel ke "Environment Variables" se config uthayega
+// 👇 FIX: "export default" hata kar "module.exports"
+module.exports = (req, res) => {
   const config = process.env.FIREBASE_CONFIG_JSON;
 
   if (!config) {
     return res.status(500).json({ error: "Config Missing in Vercel" });
   }
 
-  // Frontend ko Config JSON bhejo
   res.status(200).json(JSON.parse(config));
-}
+};
